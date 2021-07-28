0e115b1eb7c512926628ca5a72b8d2241a7f90d0
Balazs Mark <contact@mark-balazs.com>
2021-07-28 22:59:39 +0200

    BUGFIX: Handle network connection error during the updating

--- modules/check_updates.py
+++ modules/check_updates.py
@@ -17,8 +17,15 @@ class CheckUpdate:
     name_of_project = "szitakoto-elemzo-program"
 
     def __init__(self):
-        self.latest_release_version = self.get_latest_release_number()
-        self.print_result()
+        print()
+        print("[ ] Frissítés keresése...")
+        try:
+            self.latest_release_version = self.get_latest_release_number()
+            self.print_result()
+        except:
+            print("\n[-] HIBA: A program verziójának frissessége hálózati hiba miatt nem ellenőrizhető. (valószínűleg a céges tűzfal blokkolja a frissítést)")
+            print(f"    A legfrisebb verziót a {self.url_of_latest_release} oldalon eléred.")
+
 
     def get_latest_release_number(self):
         self.url_of_latest_release = f"https://github.com/{self.username_of_developer}/{self.name_of_project}/releases/latest"
@@ -26,6 +33,7 @@ class CheckUpdate:
         redirected_url = response.url
         latest_release_version = redirected_url.split("/")[-1]
         return latest_release_version
+
 
     def print_result(self):
         if self.version_in_use == self.latest_release_version:
